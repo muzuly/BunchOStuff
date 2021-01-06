acdaff3267de00b6bf568d2dae48dffeeafc65dc
muzuly <matthewbogen06@gmail.com>
2021-01-06 15:15:31 -0600

    Gold Ring

--- src/main/java/io/github/muzuly/bunchostuff/core/init/ItemInit.java
+++ src/main/java/io/github/muzuly/bunchostuff/core/init/ItemInit.java
@@ -21,6 +21,9 @@ public class ItemInit {
 	public static final RegistryObject<Item> IRON_RING_ITEM = ITEMS.register("iron_ring_item",
 			() -> new Item(new Item.Properties().group(ItemGroup.MISC).maxStackSize(1)));
 	
+	public static final RegistryObject<Item> GOLD_RING_ITEM = ITEMS.register("gold_ring_item",
+			() -> new Item(new Item.Properties().group(ItemGroup.MISC).maxStackSize(1)));
+	
 	// BLOCK ITEMS
 	public static final RegistryObject<BlockItem> TEST_BLOCK = ITEMS.register("test_block", 
 			() -> new BlockItem(BlockInit.TEST_BLOCK.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
